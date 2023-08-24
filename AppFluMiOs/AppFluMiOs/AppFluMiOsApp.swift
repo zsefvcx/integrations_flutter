beb40bf4075e8fee995c0699082def8daa35034d
zsefvcx <zsefvcx@list.ru>
2023-08-24 01:44:08 -0700

    lesson19 task4 ios init

---
+++ AppFluMiOs/AppFluMiOs/AppFluMiOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppFluMiOsApp.swift
+//  AppFluMiOs
+//
+//  Created by Виктор Д on 24.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppFluMiOsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
